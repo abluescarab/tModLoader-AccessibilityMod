644c73b80b65b29c998a3c8c9897526eb0ad2644
Alana Gilston <abluescarab@users.noreply.github.com>
2023-03-11 19:35:47 -0800

    Save panel location with character

--- AccessibilityModPlayer.cs
+++ AccessibilityModPlayer.cs
@@ -1,14 +1,27 @@
-﻿using Terraria.ModLoader;
+﻿using CustomSlot;
+using Terraria;
+using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
 namespace AccessibilityMod {
     public class AccessibilityModPlayer : ModPlayer {
+        private PlayerData<float> panelX = 
+            new PlayerData<float>("panelX", AccessibilityModUI.DefaultCoordinates.X);
+        private PlayerData<float> panelY = 
+            new PlayerData<float>("panelY", AccessibilityModUI.DefaultCoordinates.Y);
+
         public override void LoadData(TagCompound tag) {
-            base.LoadData(tag);
+            panelX.Value = tag.GetFloat(panelX.Tag);
+            panelY.Value = tag.GetFloat(panelY.Tag);
         }
 
         public override void SaveData(TagCompound tag) {
-            base.SaveData(tag);
+            tag.Add(panelX.Tag, AccessibilityModSystem.UI.Panel.Left.Pixels);
+            tag.Add(panelY.Tag, AccessibilityModSystem.UI.Panel.Top.Pixels);
+        }
+
+        public override void OnEnterWorld(Player player) {
+            AccessibilityModSystem.UI.SetPosition(panelX.Value, panelY.Value);
         }
 
         public override void PostUpdate() {
